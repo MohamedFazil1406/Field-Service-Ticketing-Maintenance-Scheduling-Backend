049e1a708aac09ea7820deacf4f4c16d399bb1e7
MohamedFazil1406 <mohamedfazil01406@gmail.com>
2026-08-18 19:30:38 +0530

    docs: add OpenAPI documentation

---
+++ src/main/java/com/nova/fieldops/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package com.nova.fieldops.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI fieldOpsOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("FieldOps API")
+                        .version("1.0")
+                        .description(
+                                "Field operations management API for users, sites, devices and tickets."
+                        ))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        "bearerAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
+}
